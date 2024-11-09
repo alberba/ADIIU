154061e488b1c7cd775397bf7d4922947c1ca78b
alberba <albertsalomv@gmail.com>
2024-11-09 19:12:31 +0100

    Mejora la consistencia del formato y corrige la estructura del código en database.php

--- database.php
+++ database.php
@@ -1,28 +1,26 @@
 <?php
-  header('Content-Type: application/json');
+header("Content-Type: application/json");
 
-  $hola = $_GET['hola'];
-  $conexion = mysqli_connect("localhost","root","");
-  $bd = mysqli_select_db($conexion, "imdb_db");
-  if($hola == "decadas"){
+$hola = $_GET["hola"];
+$conexion = mysqli_connect("localhost", "root", "");
+$bd = mysqli_select_db($conexion, "imdb_db");
+if ($hola == "decadas") {
     $sql = "SELECT FLOOR(Released_Year / 10) * 10 AS decade, 
-                COUNT(*) AS movie_count
-            FROM 
-                imdb_top_1000
-            GROUP BY 
-                decade
-            HAVING
-                decade > 0
-            ORDER BY 
-                decade;";
-    
+            COUNT(*) AS movie_count
+        FROM 
+            imdb_top_1000
+        GROUP BY 
+            decade
+        HAVING
+            decade > 0
+        ORDER BY 
+            decade;";
+
     $result = mysqli_query($conexion, $sql);
 
     echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
-  } else {
+} else {
     echo "No se encontro la opcion";
-  }
-    
-    mysqli_close($conexion);
-  
-?> 
+}
+
+mysqli_close($conexion);
